79abad0816023d7b4091c3d87abd4cc3f9c8ae17
bkoning23 <koningb@mail.gvsu.edu>
2015-04-10 19:02:19 -0700

    Created some data structures

--- Controller.py
+++ Controller.py
@@ -1,14 +1,30 @@
 __author__ = 'Brendan'
 
 import sys, getopt
+from Model import stats, process
 
 def main(argv):
+	stat = stats()
+	#int array saying which pids have already been made
+	pages = []
+	#Model.process array, tuple of the format [pid, pcb]
+	pcb = []
 	with open(argv[0], 'r') as inputfile:
 		for line in inputfile:
-			
-
-			print line.split(":")[0][1]
-			print int(line.split(":")[1].strip(), 2)
+			#Process ID
+			pid = line.split(":")[0][1]
+			#Requested Page
+			page = int(line.split(":")[1].strip(), 2)
+			if(pid in pages):
+				#This is the index of the tuple that contains the 
+				#pcb corresponding to the pid
+				a = [x[0] for x in pcb].index(pid)
+				b = pcb[a][1].add(page)
+			else:
+				pcb.append((pid, process(pid)))
+				pages.append(pid)
+				print(pcb)
+				print(pages)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
--- Model.py
+++ Model.py
@@ -1,1 +1,31 @@
 __author__ = 'Brendan'
+
+class stats:
+	def __init__(self):
+		self.proceses = []
+
+	def add(self, id):
+		self.proceses.append(id)
+
+	def count(self):
+		return len(set(self.proceses))
+
+class process:
+	def __init__(self, id):
+		self.refcount = 0
+		self.pages = []
+		self.pid = id
+
+	def add(self, page):
+		self.pages.append(page)
+		self.refcount += 1
+
+	def getpages(self):
+		return self.pages
+
+	def getrefcount(self):
+		return self.refcount
+
+	def __str__(self):
+		return self.pages
+
