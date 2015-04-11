4e855d935dfdb2c9deb2d20befe5a85e3f7da72d
bkoning23 <koningb@mail.gvsu.edu>
2015-04-10 18:12:55 -0700

    Gets process number and page requested

--- Controller.py
+++ Controller.py
@@ -2,12 +2,13 @@ __author__ = 'Brendan'
 
 import sys, getopt
 
-
 def main(argv):
-    inputfile = open(argv[2], 'r')
-    print (inputfile)
+	with open(argv[0], 'r') as inputfile:
+		for line in inputfile:
+			
 
-
+			print line.split(":")[0][1]
+			print int(line.split(":")[1].strip(), 2)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+	main(sys.argv[1:])
