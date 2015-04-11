de6697a7cdfc57abf8ab959938083c2b66434291
bkoning23 <koningb@mail.gvsu.edu>
2015-04-11 10:36:01 -0700

    Most of B level done, each process still needs their page table

--- Controller.py
+++ Controller.py
@@ -1,21 +1,51 @@
 __author__ = 'Brendan'
 
 import sys, getopt
-from Model import stats, process
+from Model import stats, process, freeframelist, mainmemory
 
 def main(argv):
 	stat = stats()
 	#int array saying which pids have already been made
-	pages = []
-	#Model.process array, tuple of the format [pid, pcb]
+	pids = []
+	#Model.process array, tuple of the format [pid, pcb]. This is a collection of PCBs that the OS manages
 	pcb = []
+	#Free frame list
+	freeframes = freeframelist(16)
+	#Memory
+	memory = mainmemory(16)
 	with open(argv[0], 'r') as inputfile:
 		for line in inputfile:
 			#Process ID
 			pid = line.split(":")[0][1]
 			#Requested Page
 			page = int(line.split(":")[1].strip(), 2)
-			if(pid in pages):
+			print ("Process " + str(pid) + " accessed Page " + str(page))
+			
+			#check if page is in memory
+			if(memory.isinmemory(pid, page)):
+				print("ITS IM MEMEORY")
+			else:
+				print ("NOT IN MEMORY")
+				try:	
+					memory.addtomemory(pid, page)
+					print("adding " + str(page) + " " + str(pid))
+				except mainmemory.MemError:
+					print ("OUT OF MEMORY LRU REPLACE")
+					memory.lru(pid, page)
+
+			memory.topofstack(pid, page)
+			print(memory.getstack())
+				
+
+			#if(freeframes.isempty()):
+				#print(freeframes.getfreesize())
+				#print "HELLO"
+			#else:
+				#print "Page Fault"
+				#print (freeframes.allocate())
+
+
+			if(pid in pids):
 				#This is the index of the tuple that contains the 
 				#pcb corresponding to the pid
 				a = [x[0] for x in pcb].index(pid)
@@ -26,8 +56,8 @@ def main(argv):
 			else:
 				#This is if the process has not been recorded yet
 				pcb.append((pid, process(pid)))
-				pages.append(pid)
-			print ("Process " + str(pid) + " accessed Page " + str(page))
+				pids.append(pid)
+			
 
 		for i in pcb:
 			print("Process " + str(i[0]) + " referenced " + str(i[1].getrefcount()) + " times.")
--- Model.py
+++ Model.py
@@ -1,5 +1,7 @@
 __author__ = 'Brendan'
 
+from collections import deque
+
 class stats:
 	def __init__(self):
 		self.proceses = []
@@ -34,4 +36,81 @@ class process:
 
 	def __str__(self):
 		return str(self.refcount)
+
+class freeframelist:
+	def __init__(self, size):
+		self.freeframes = range(size)
+
+	def getfree(self):
+		return self.freeframes
+
+	def getfreesize(self):
+		return len(self.freeframes)
+
+	def isempty(self):
+		if not self.freeframes:
+			return True
+		return False
+
+	def allocate(self):
+		return self.freeframes.pop()
+
+class mainmemory:
+	class MemError(Exception):
+		def __init__(self, value):
+			self.value = value
+		def __str__(self):
+			return repr(self.value)
+
+	def __init__(self, total):
+		self.memory = []
+		#In refstack, left is most recently used, right 
+		#is what will be removed if needed
+		self.refstack = deque([])
+		self.size = total
+		self.e = None
+
+	def getmem(self):
+		return self.memory
+
+	def isinmemory(self, pid, page):
+		return (pid, page) in self.memory
+
+	def getindex(self, pid, page):
+		try:
+			return (self.memory.index((pid, page)))
+		except ValueError, ex:
+			self. e = ex
+
+	def topofstack(self, pid, page):
+		index = self.getindex(pid, page)
+		if not self.e:
+			try:
+				self.refstack.remove(index)
+			except ValueError:
+				pass
+			self.refstack.appendleft(self.getindex(pid, page))
+
+	def addtomemory(self, pid, page):
+		if(len(self.memory) >= self.size):
+			raise self.MemError("Out of Memory")
+		self.memory.append((pid, page))
+
+	def getstack(self):
+		return self.refstack
+
+	#Determines the LRU page and replaces it	
+	def lru(self, pid, page):
+		#Index of main mem page to be replaced
+		index = self.refstack.pop()
+		#Removes the frame at the specified index and replaces it
+		self.memory.pop(index)
+		self.memory.insert(index, (pid, page))	
+
+
+
+
+	
+
+
 
